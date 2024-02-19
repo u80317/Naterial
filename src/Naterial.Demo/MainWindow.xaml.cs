ad4e15bd92a010f06821e2999b5623829caf0bff
u80317 <160233598+u80317@users.noreply.github.com>
2024-02-19 22:36:04 +0300

    feat: add base of solution projects

---
+++ src/Naterial.Demo/MainWindow.xaml.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace Naterial.Demo
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+    }
+}
---
+++ src/Naterial/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Markup;
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,
+    ResourceDictionaryLocation.SourceAssembly
+)]
+
+[assembly: XmlnsPrefix("http://schemas.naterial.com/winfx/xaml", "nd")]
+
+[assembly: ComVisible(false)]
