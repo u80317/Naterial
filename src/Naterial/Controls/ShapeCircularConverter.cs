63352f9b56a35a33a47f40abd709584e2df5a103
u80317 <160233598+u80317@users.noreply.github.com>
2024-02-23 16:41:16 +0300

    feat: add basic shape converters

---
+++ src/Naterial/Controls/ShapeCircularConverter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Naterial.Controls
+{
+    /// <summary>
+    /// Represents the converter that converts the element dimensions to <see cref="CornerRadius" />
+    /// of the circular shape.
+    /// </summary>
+    public sealed class ShapeCircularConverter : IMultiValueConverter
+    {
+        /// <summary>
+        /// Represents instance of converter that provides access from XAML.
+        /// </summary>
+        public static IMultiValueConverter Default { get; } = new ShapeCircularConverter();
+
+
+        /// <summary>
+        /// Converts the element dimensions to <see cref="CornerRadius" /> of the circular shape.
+        /// </summary>
+        /// <param name="values">
+        /// The <see cref="FrameworkElement.ActualWidth" /> and <see cref="FrameworkElement.ActualHeight" />.
+        /// </param>
+        /// <param name="targetType">
+        /// This parameter is not used.
+        /// </param>
+        /// <param name="parameter">
+        /// This parameter is not used.
+        /// </param>
+        /// <param name="culture">
+        /// This parameter is not used.
+        /// </param>
+        /// <returns>
+        /// The <see cref="CornerRadius" /> of the circular shape.
+        /// </returns>
+        /// <remarks>
+        /// The <see cref="CornerRadius" /> is equal to value of element small side divided by 2.
+        /// </remarks>
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length == 2 && values[0] is double width && values[1] is double height)
+            {
+                return new CornerRadius(Math.Min(width, height) / 2);
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+
+        /// <summary>
+        /// Not Supported.
+        /// </summary>
+        /// <param name="value">
+        /// This parameter is not used.
+        /// </param>
+        /// <param name="targetTypes">
+        /// This parameter is not used.
+        /// </param>
+        /// <param name="parameter">
+        /// This parameter is not used.
+        /// </param>
+        /// <param name="culture">
+        /// This parameter is not used.
+        /// </param>
+        /// <exception cref="NotSupportedException">
+        /// The method was called.
+        /// </exception>
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+            => throw new NotSupportedException();
+    }
+}
---
+++ src/Naterial/Controls/ShapeClipConverter.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Naterial.Controls
+{
+    /// <summary>
+    /// Represents the converter that converts the element dimensions to <see cref="Geometry" />
+    /// of the clipping shape.
+    /// </summary>
+    public sealed class ShapeClipConverter : IMultiValueConverter
+    {
+        /// <summary>
+        /// Represents instance of converter that provides access from XAML.
+        /// </summary>
+        public static IMultiValueConverter Default { get; } = new ShapeClipConverter();
+
+
+        /// <summary>
+        /// Converts the element dimensions to <see cref="Geometry" /> of the clipping shape.
+        /// </summary>
+        /// <param name="values">
+        /// The <see cref="FrameworkElement.ActualWidth" />, <see cref="FrameworkElement.ActualHeight" />
+        /// and <see cref="CornerRadius" />.
+        /// </param>
+        /// <param name="targetType">
+        /// This parameter is not used.
+        /// </param>
+        /// <param name="parameter">
+        /// This parameter is not used.
+        /// </param>
+        /// <param name="culture">
+        /// This parameter is not used.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Geometry" /> of the clipping shape.
+        /// </returns>
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length == 3 && values[0] is double width &&
+                                      values[1] is double height &&
+                                      values[2] is CornerRadius radius)
+            {
+                var rectangle = new RectangleGeometry(
+                    new Rect(0, 0, width, height), radius.TopLeft, radius.TopLeft
+                );
+                rectangle.Freeze();
+
+                return rectangle;
+            }
+
+            return DependencyProperty.UnsetValue;
+        }
+
+        /// <summary>
+        /// Not Supported.
+        /// </summary>
+        /// <param name="value">
+        /// This parameter is not used.
+        /// </param>
+        /// <param name="targetTypes">
+        /// This parameter is not used.
+        /// </param>
+        /// <param name="parameter">
+        /// This parameter is not used.
+        /// </param>
+        /// <param name="culture">
+        /// This parameter is not used.
+        /// </param>
+        /// <exception cref="NotSupportedException">
+        /// The method was called.
+        /// </exception>
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+            => throw new NotSupportedException();
+    }
+}
