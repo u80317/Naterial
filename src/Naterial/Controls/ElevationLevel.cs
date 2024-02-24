a3d696484f416edc42632fce657741be9192fb79
u80317 <160233598+u80317@users.noreply.github.com>
2024-02-24 12:27:33 +0300

    feat: add elevation values & assist

---
+++ src/Naterial/Controls/ElevationLevel.cs
@@ -0,0 +1,37 @@
+﻿namespace Naterial.Controls
+{
+    /// <summary>
+    /// Declares the elevation levels of an element along the z-axis.
+    /// </summary>
+    public enum ElevationLevel
+    {
+        /// <summary>
+        /// Use an elevation level equal to 0dp. Default.
+        /// </summary>
+        Level0,
+        /// <summary>
+        /// Use an elevation level equal to 1dp.
+        /// </summary>
+        Level1,
+        /// <summary>
+        /// Use an elevation level equal to 2dp.
+        /// </summary>
+        Level2,
+        /// <summary>
+        /// Use an elevation level equal to 4dp.
+        /// </summary>
+        Level3,
+        /// <summary>
+        /// Use an elevation level equal to 6dp.
+        /// </summary>
+        Level4,
+        /// <summary>
+        /// Use an elevation level equal to 8dp.
+        /// </summary>
+        Level5,
+        /// <summary>
+        /// Use an elevation level equal to 12dp.
+        /// </summary>
+        Level6,
+    }
+}
